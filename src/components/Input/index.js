ee3e085f91a4c4e317364e6513d2ecee3ba82741
vednoob <leduchyy@gmail.com>
2023-02-17 01:35:08 +0700

    Logic: 2waybinding, add

---
+++ src/components/Input/index.js
@@ -0,0 +1,60 @@
+import { useState, useRef, useEffect } from "react";
+
+function Input() {
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState(
+    (function () {
+      const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+      return storageJobs;
+    })() ?? []
+  );
+
+  const inputRef = useRef(null);
+
+  const handleSubmit = () => {
+    if (job === "") {
+      return;
+    }
+    setJobs((prev) => {
+      const newJobs = [...prev, job];
+
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem("jobs", jsonJobs);
+
+      return newJobs;
+    });
+    setJob("");
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {}, []);
+
+  const handleDelete = (index) => {
+    const jsonJobs = JSON.parse(localStorage.getItem("jobs"));
+
+    const newJobs = JSON.stringify(jsonJobs.splice(index, 1));
+
+    localStorage.setItem("jobs", newJobs);
+  };
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        value={job}
+        onChange={(e) => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>
+            {job}
+            <span onClick={() => handleDelete(index)}>&times;</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Input;
